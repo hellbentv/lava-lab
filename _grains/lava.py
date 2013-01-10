4427667b3795ae2f73fad18294d710a20f5d7597
Andy Doan <andy.doan@linaro.org>
2013-01-10 10:37:55 -0600

    make lava grain work if no instances exist

--- _grains/lava.py
+++ _grains/lava.py
@@ -4,8 +4,9 @@ import os
 def instances():
     insts = []
     instdir = '/srv/lava/instances'
-    for f in os.listdir(instdir):
-        p = os.path.join(instdir, f)
-        if os.path.isdir(p):
-            insts.append(p)
+    if os.path.exists(instdir):
+        for f in os.listdir(instdir):
+            p = os.path.join(instdir, f)
+            if os.path.isdir(p):
+                insts.append(p)
     return {'lava_instances': insts}
